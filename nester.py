eb9097d5c1b0aa8b5d78950ccb7956f2a4c15569
armzig <bellotaophyc@outlook.com>
2023-01-19 20:10:16 +0100

    19 01 2023 Importing the created module. Commenting out the movies list and last print statement

--- nester.py
+++ nester.py
@@ -1,9 +1,11 @@
 """This is the "nester.py" module, and it provides one function called print_lol() which prints lists that may or may not include nested lists."""
 
 #!/usr/bin/python3
+"""
 movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
 ["Graham Chapman", ["Michael Palin", "John Cleese",
 "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+"""
 
 def print_lol(the_list):
 
@@ -14,4 +16,4 @@ def print_lol(the_list):
         else:
             print(each_item)
         
-print_lol(movies)
+#print_lol(movies)
