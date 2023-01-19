923f94b007da30f9429910fdfd199ab9bfc6fa1a
armzig <bellotaophyc@outlook.com>
2023-01-19 19:00:18 +0100

    19 01 2023 The metadata about my module.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'Seun',
+    author_email = 'bellotaophyc@outlook.com',
+    url = 'https://github.com/LaVie-environment',
+    description = 'A simple printer pf nested lists',
+)
